48f1c255295ba9aa3c716c64bfdfa0757ae2abc6
trey-causey <trey.causey@outlook.com>
2020-03-06 12:38:21 -0500

    mid working on #1313 issue

--- src/Sandbox.Prod/Solution.cs
+++ src/Sandbox.Prod/Solution.cs
@@ -1,11 +1,34 @@
 
 using System;
+using System.Collections;
 
 namespace Sandbox.Prod 
 {
     public class Solution
     {
+        public int[] DecompressRLElist(int[] nums) 
+        {
+            int i = (nums.Length % 2);
+            Stack thisStack = new Stack();
+            if (i == 0 && (nums.Length >=2 && nums.Length <=100))
+            {
+                
+                while(i < (nums.Length))
+                {
+                    for(int x = 0; x < nums[i]; i*=2 )
+                    {
+                        thisStack.Push(i + 1);
+                    }
+
+                    //the work
+
+                }
+                
+            }        
+           
+            return thisStack.CopyTo(nums, 0);
         
+        }
     }
 
 }
--- test/Sandbox.Tests/SolutionTests.cs
+++ test/Sandbox.Tests/SolutionTests.cs
@@ -10,14 +10,16 @@ namespace Sandbox.Tests
         public void Test1()
         {
             //arrange
-           
+           int[] expectedArray1 = {2,4,4,4};
+           int[] actualArray1 = {};
+           int[] nums1 = {1,2,3,4};
+           Solution arrayObject1 = new Solution();
            
             //act
             
-
+            actualArray1 = arrayObject1.DecompressRLElist(nums1);
             //assert
-            
+            Assert.Equal(expectedArray1, actualArray1);
             
         }
     }
-}
